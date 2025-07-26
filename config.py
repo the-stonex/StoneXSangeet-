7e3dab811d60b9e533bc36ed015211ae8499874b
The-stone <147587252+the-stonex@users.noreply.github.com>
2025-07-26 07:45:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 16000))
 # ________________________________________________________________________________#
 # Chat id of a group for logging bot's activities
-LOGGER_ID = int(getenv("LOGGER_ID",))
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", None))
 # ________________________________________________________________________________#
 # Get this value from  on Telegram by /id
 OWNER_ID = int(getenv("OWNER_ID"))
