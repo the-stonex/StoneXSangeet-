b189d1eee1399191b0bc2f6d1b4933ec33fb2236
The-stone <147587252+the-stonex@users.noreply.github.com>
2025-07-28 14:16:04 +0530

    Update help.py

--- YTMUSIC/utils/inline/help.py
+++ YTMUSIC/utils/inline/help.py
@@ -2,7 +2,7 @@ from typing import Union
 
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-from YTMUSIC import app
+from AviaxMusic import app
 
 
 def help_pannel(_, START: Union[bool, int] = None):
